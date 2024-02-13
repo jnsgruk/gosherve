81ebfd9224e752c191745885cd50f51d59368e36
Jon Seager <jon@sgrs.uk>
2024-02-13 09:09:34 +0000

    refactor: simplification of routeHandler logic

--- pkg/server/route_handler.go
+++ pkg/server/route_handler.go
@@ -19,31 +19,45 @@ func (s *Server) routeHandler(w http.ResponseWriter, r *http.Request) {
 	s.metrics.requestsTotal.Inc()
 	l := logging.GetLoggerFromCtx(r.Context())
 
+	// First check if there is a redirect defined, and serve it if there is.
+	if redirected := handleRedirect(w, r, s); redirected {
+		return
+	}
+
+	// If there is no webroot, and the redirect isn't defined, then 404.
 	if s.webroot == nil {
-		handleRedirect(w, r, s)
+		handleNotFound(w, r, s)
 		return
 	}
 
 	var filepath string
 	if r.URL.Path == "/" {
+		// Serve "index.html" if someone browses to the root
 		filepath = "index.html"
 	} else {
 		filepath = strings.TrimPrefix(r.URL.Path, "/")
 		filepath = strings.TrimSuffix(filepath, "/")
 	}
 
+	// Stat the file and report not found if that fails
+	fi, err := fs.Stat(*s.webroot, filepath)
+	if err != nil {
+		handleNotFound(w, r, s)
+		return
+	}
+
+	// If the file is a directory, append "index.html" to the end
+	if fi.IsDir() {
+		filepath = fmt.Sprintf("%s/index.html", filepath)
+	}
+
 	// First try reading the a file at the exact path sent
-	var b []byte
 	b, err := fs.ReadFile(*s.webroot, filepath)
 	if err != nil {
-		// If that fails, check that it isn't a link to a directory with an index.html
-		b, err = fs.ReadFile(*s.webroot, fmt.Sprintf("%s/index.html", filepath))
-		if err != nil {
-			// The URL doesn't represent a valid file, treat request as a redirect
-			handleRedirect(w, r, s)
-			return
-		}
+		handleNotFound(w, r, s)
+		return
 	}
+
 	w.Header().Set("Cache-Control", "public, max-age=31536000, must-revalidate")
 	w.Header().Set("ETag", fmt.Sprintf(`"%d-%x"`, len(b), sha1.Sum(b)))
 
@@ -54,15 +68,14 @@ func (s *Server) routeHandler(w http.ResponseWriter, r *http.Request) {
 
 // handleRedirect tries to lookup a redirect by its alias, returning the HTTP 301
 // response if found.
-func handleRedirect(w http.ResponseWriter, r *http.Request, s *Server) {
+func handleRedirect(w http.ResponseWriter, r *http.Request, s *Server) bool {
 	l := logging.GetLoggerFromCtx(r.Context())
 
 	alias := strings.TrimPrefix(r.URL.Path, "/")
 
 	url, err := s.LookupRedirect(alias)
 	if err != nil {
-		handleNotFound(w, r, s)
-		return
+		return false
 	}
 
 	s.metrics.redirectsServed.WithLabelValues(alias).Inc()
@@ -73,32 +86,32 @@ func handleRedirect(w http.ResponseWriter, r *http.Request, s *Server) {
 
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	http.Redirect(w, r, url, http.StatusMovedPermanently)
+
+	return true
 }
 
 // handleNotFound handles invalid paths/redirects and returns a 404.html or plaintext "Not found"
 func handleNotFound(w http.ResponseWriter, r *http.Request, s *Server) {
 	l := logging.GetLoggerFromCtx(r.Context())
-	logPlainResponse := func() {
+	s.metrics.responseStatus.WithLabelValues(strconv.Itoa(http.StatusNotFound)).Inc()
+
+	plainNotFound := func() {
+		http.Error(w, "Not found", http.StatusNotFound)
 		l.Error("not found", slog.Group("response", "status_code", http.StatusNotFound, "text", "Not found"))
 	}
 
-	w.WriteHeader(http.StatusNotFound)
-	s.metrics.responseStatus.WithLabelValues(strconv.Itoa(http.StatusNotFound)).Inc()
-
 	if s.webroot == nil {
-		w.Write([]byte("Not found"))
-		logPlainResponse()
+		plainNotFound()
 		return
 	}
 
 	// Check if there is a 404.html to return, otherwise return plaintext
 	content, err := fs.ReadFile(*s.webroot, "404.html")
 	if err != nil {
-		w.Write([]byte("Not found"))
-		logPlainResponse()
+		plainNotFound()
 		return
 	}
 
-	w.Write(content)
+	http.Error(w, string(content), http.StatusNotFound)
 	l.Error("not found", slog.Group("response", "status_code", http.StatusNotFound, "file", "404.html"))
 }
